e1bbbdce4fd0bf076007ab2f62e8e859ac59709b
rex <hhgfdzjokhg@gmail.com>
2026-03-23 21:46:46 +0800

    [功能]安装了所有的依赖包并导出了环境

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from PIL import Image
+ 
+# 设置中文字体（可选，避免中文乱码）
+plt.rcParams['font.sans-serif'] = ['SimHei']  
+plt.rcParams['axes.unicode_minus'] = False
